69f00c6dbd7aea8c576c1f7989731b95aad95da5
Ezequiel <ezequiel3434@gmail.com>
2020-07-16 16:21:53 -0300

    initial commit, adding TableViewController

---
+++ WordsStorage/TableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TableViewController.swift
+//  WordsStorage
+//
+//  Created by Ezequiel Parada Beltran on 16/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var wordsTable: [String] = ["Ezequiel", "ALexis", "Karen"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return wordsTable.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = wordsTable[indexPath.row]
+
+        return cell
+    }
+    
+
+   
+}
