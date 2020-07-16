38901fc237f3b78a0153781a11a4722a4c538819
Ezequiel <ezequiel3434@gmail.com>
2020-07-16 16:02:43 -0300

    Initial Commit

---
+++ WordsStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordsStorage
+//
+//  Created by Ezequiel Parada Beltran on 16/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
